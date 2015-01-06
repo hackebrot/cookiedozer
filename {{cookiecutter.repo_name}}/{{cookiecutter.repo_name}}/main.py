67cfe5ef5613f8a0ee8a4d219cd6b4583a578646
Raphael Pierzina <raphael@hackebrot.de>
2015-01-06 14:25:28 +0100

    Set up main.py to run the app

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from {{cookiecutter.repo_name}} import {{cookiecutter.app_class_name}}
+
+
+if __name__ == '__main__':
+    {{cookiecutter.app_class_name}}().run()
