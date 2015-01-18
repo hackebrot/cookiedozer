2ed1ce9c2b116497af1b1c2a97b7382d760cadca
Raphael Pierzina <raphael@hackebrot.de>
2015-01-18 17:55:16 +0100

    Implement basic setup.py with cookiecutter variables

---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "{{cookiecutter.repo_name}}",
+    version = "{{cookiecutter.version}}",
+    author = "{{cookiecutter.full_name}}",
+    author_email = "{{cookiecutter.email}}",
+    description = "{{cookiecutter.short_description}}",
+    license = "MIT",
+    keywords = "example documentation tutorial",
+    url = "https://github.com/{{cookiecutter.github_username}}/{{cookiecutter.repo_name}}",
+    packages=['{{cookiecutter.repo_name}}'],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Artistic Software',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+)
