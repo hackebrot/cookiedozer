a208c090f2e80680afc325eb80ec593ca0db0e7c
Raphael Pierzina <raphael@hackebrot.de>
2015-01-18 18:18:28 +0100

    Insert missing imports

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,5 +1,9 @@
+import sys
 import os
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
