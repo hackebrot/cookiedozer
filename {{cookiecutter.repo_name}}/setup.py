9f7756ff7431cc38bef5f054565eb2fa439d5a23
Raphael Pierzina <raphael@hackebrot.de>
2015-01-18 18:15:32 +0100

    Insert PyTest command to run the suite from setup.py

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -4,6 +4,25 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
 setup(
     name = "{{cookiecutter.repo_name}}",
     version = "{{cookiecutter.version}}",
@@ -28,6 +47,8 @@ setup(
             '{{cookiecutter.repo_name}}={{cookiecutter.repo_name}}.main:main'
         ]
     },
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: X11 Applications',
