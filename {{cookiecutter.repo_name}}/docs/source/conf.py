e5bdeb4bfe2367df66b1851310638891b89918b7
Raphael Pierzina <raphael@hackebrot.de>
2015-02-21 19:08:41 +0100

    Add link to kivy docs for intersphinx

--- {{cookiecutter.repo_name}}/docs/source/conf.py
+++ {{cookiecutter.repo_name}}/docs/source/conf.py
@@ -263,4 +263,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    'http://kivy.org/docs/': None,
+}
