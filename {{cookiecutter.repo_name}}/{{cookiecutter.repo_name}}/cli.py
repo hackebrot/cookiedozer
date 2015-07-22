f9da8c4aa061223dac5147f6eaec6ad3419d1d6a
Raphael Pierzina <raphael@hackebrot.de>
2015-07-22 11:07:06 +0200

    Add cli module to accept a language option

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/cli.py
@@ -0,0 +1,17 @@
+import os
+os.environ["KIVY_NO_ARGS"] = "1"
+
+import click
+
+from {{cookiecutter.repo_name}}.{{cookiecutter.repo_name}} import {{cookiecutter.app_class_name}}
+
+
+@click.command()
+@click.option(
+    '-l', '--language', help='Default language of the App', default='en',
+    type=click.Choice(['en', 'de'])
+)
+def main(language):
+    """Run {{cookiecutter.app_class_name}} with the given language setting.
+    """
+    {{cookiecutter.app_class_name}}(language).run()
